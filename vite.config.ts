17b36155c9efb736f268da358caa2b3608a5b3ef
forumetecab <forumtecinter@gmail.com>
2023-10-24 18:17:56 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()]
+  plugins: [react(), viteTsconfigPaths()],
+  
 })
